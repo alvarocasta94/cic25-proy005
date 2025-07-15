f4ae41678259daf546507491aa1f676c634a9ce1
nomail <no@nomail.no>
2025-07-15 12:23:51 +0200

    Resto de métodos del CRUD y un ejemplo de test de integración del DELETE

--- src/main/java/es/cic/curso25/proy005/TelefonoController.java
+++ src/main/java/es/cic/curso25/proy005/TelefonoController.java
@@ -3,8 +3,12 @@ package es.cic.curso25.proy005;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/telefono")
 public class TelefonoController {
 
-    // private int contador = 0;
+    private int contador = 0;
 
     // private Telefono telefono;
     // C R U D
+
+    @PostMapping
+    public long create(@RequestBody Telefono telefono) {
+        return ++contador;
+    }
+
 
 
     @GetMapping
@@ -56,4 +66,14 @@ public class TelefonoController {
         return telefono1;
     }
 
+    @PutMapping
+    public void update(@RequestBody Telefono telefono) {
+        // Falta de escribir todo
+        throw new UnsupportedOperationException();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        contador = contador;
+    }
 }
--- src/test/java/es/cic/curso25/proy005/Proy005ApplicationTests.java
+++ src/test/java/es/cic/curso25/proy005/Proy005ApplicationTests.java
@@ -3,14 +3,30 @@ package es.cic.curso25.proy005;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Proy005ApplicationTests {
 
+	@Autowired
+    private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testTelefonoDelete() throws Exception {
+		mockMvc
+			.perform(delete("/telefono/4"))
+			.andDo(print())
+            .andExpect(status().isOk())
+			.andReturn();
 	}
 
 
